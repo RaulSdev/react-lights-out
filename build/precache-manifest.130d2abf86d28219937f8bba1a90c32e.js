bdd06cc04dfa33d1d614205eac7295ba8d0465fd
Raul <raulsdev@gamil.com>
2021-09-16 14:09:11 -0500

    run deploy

---
+++ build/precache-manifest.130d2abf86d28219937f8bba1a90c32e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e9b27f6952fabe64e27",
+    "url": "/RaulSdev/react-lights-out/static/css/main.81a96f58.chunk.css"
+  },
+  {
+    "revision": "4e9b27f6952fabe64e27",
+    "url": "/RaulSdev/react-lights-out/static/js/main.4e9b27f6.chunk.js"
+  },
+  {
+    "revision": "024dd0e473331f56a349",
+    "url": "/RaulSdev/react-lights-out/static/js/1.024dd0e4.chunk.js"
+  },
+  {
+    "revision": "c004447bf6d218c1b84d",
+    "url": "/RaulSdev/react-lights-out/static/js/runtime~main.c004447b.js"
+  },
+  {
+    "revision": "a1f5d8183d1524c597dcd540c237fdba",
+    "url": "/RaulSdev/react-lights-out/index.html"
+  }
+];
